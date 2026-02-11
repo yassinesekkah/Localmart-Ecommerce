60cc16b309c674d33e8879424aad81c156a619c6
OUTERGA-MOUSTAFA <outergamoustafa@gmail.com>
2026-02-11 16:54:25 +0100

    feat: add disabled button on model if quantity=0 and livewire is fixed

---
+++ resources/views/livewire/product-favorites.blade.php
@@ -0,0 +1,17 @@
+<div class="absolute right-2">
+    <button wire:click="toggleFavorite"
+        class="w-12 h-12 rounded-lg flex flex-col items-center justify-center relative">
+
+        @if (!$product->isFavoriteBy(auth()->user()))
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+            d="M2.335 10.337a.5.5 0 0 1 .28-.864l6.004-.712a.5.5 0 0 0 .396-.287l2.532-5.49a.5.5 0 0 1 .908 0l2.532 5.49a.5.5 0 0 0 .395.287l6.004.712a.5.5 0 0 1 .28.864l-4.438 4.105a.5.5 0 0 0-.15.464l1.177 5.93a.5.5 0 0 1-.735.534l-5.275-2.953a.5.5 0 0 0-.488 0l-5.276 2.952a.5.5 0 0 1-.735-.533l1.178-5.93a.5.5 0 0 0-.15-.464z"/></svg>
+        @else
+       <svg class="w-6 h-6 text-yellow-400 transition-transform duration-200 hover:scale-125"
+        xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" 
+        d="M2.335 10.337a.5.5 0 0 1 .28-.864l6.004-.712a.5.5 0 0 0 .396-.287l2.532-5.49a.5.5 0 0 1 .908 0l2.532 5.49a.5.5 0 0 0 .395.287l6.004.712a.5.5 0 0 1 .28.864l-4.438 4.105a.5.5 0 0 0-.15.464l1.177 5.93a.5.5 0 0 1-.735.534l-5.275-2.953a.5.5 0 0 0-.488 0l-5.276 2.952a.5.5 0 0 1-.735-.533l1.178-5.93a.5.5 0 0 0-.15-.464z"/></svg>
+        @endif
+
+    </button>
+</div>
